a26096e52c96e5c3adad1650d84b92b7663ea7bd
VladW <VIStrelnikov94@gmail.com>
2023-05-08 20:20:14 +0300

    Add Dto Models

---
+++ UsersGroupApi/Models/user_groupDto.cs
@@ -0,0 +1,11 @@
+﻿namespace UsersGroupApi.Models
+{
+    public class user_groupDto
+    {
+        public int id { get; set; }
+
+        public int code { get; set; }
+
+        public string description { get; set; }
+    }
+}
